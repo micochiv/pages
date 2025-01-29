d80268d06a668c6f1795e3033607eccc62e5264b
micochiv <m-tanaka@gojukarado.com>
2025-01-29 16:56:24 +0900

    Add files via upload

--- nazo/script.js
+++ nazo/script.js
@@ -1,9 +1,21 @@
-// .s_07 .accordion_one
 $(function(){
-  //.accordion_oneの中の.accordion_headerがクリックされたら
-  $('.s_07 .accordion_one .accordion_header').click(function(){
-    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
-    $(this).next('.accordion_inner').slideToggle();
-    $(this).toggleClass("open");
+  // すべてのアコーディオンを対象
+  $('.s_07 .accordion_header').click(function(e){
+    e.stopPropagation(); // クリックイベントが親要素に伝播しないようにする
+
+    // クリックされた .accordion_header の次の .accordion_inner を開閉
+    let $inner = $(this).next('.accordion_inner');
+    if ($inner.is(':visible')) {
+      $inner.slideUp();
+      $(this).removeClass("open");
+    } else {
+      // 他のすべてのアコーディオンを閉じるのをやめる
+      // $('.s_07 .accordion_inner').slideUp();
+      // $('.s_07 .accordion_header').removeClass("open");
+
+      // クリックされた要素だけを開く
+      $inner.slideDown();
+      $(this).addClass("open");
+    }
   });
 });
